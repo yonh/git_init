0ddd225aa8d3faa9f60dce525d17d6296067bc7a
yonh <azssjli@163.com>
2016-02-24 13:42:53 +0800

    init

---
+++ run.sh
@@ -0,0 +1,24 @@
+check_result() {
+    if [ $1 -ne 0 ]; then
+        echo "Error: $2"
+        exit $1
+    fi
+}
+git_install() {
+	if [ -x "/usr/bin/apt-get" ]; then
+		apt-get update && apt-get install -y git
+	fi
+
+	if [ -x "/usr/bin/yum" ]; then
+		yum update && yum install -y git
+	fi
+}
+if [ ! -f "/usr/bin/git" ]; then
+	git_install
+fi
+check_result $? "install fail"
+git config --global user.email "azssjli@163.com"
+git config --global user.name "yonh"
+
+
+echo "done..."
